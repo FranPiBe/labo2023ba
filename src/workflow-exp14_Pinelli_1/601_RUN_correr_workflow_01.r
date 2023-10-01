ce5c05412d27a0546da56f27937b04d1759b8e02
FranPiBe <jfranciscopinelli@gmail.com>
2023-10-01 21:45:12 +0000

    correccion

--- src/workflow-exp14_Pinelli_1/601_RUN_correr_workflow_01.r
+++ src/workflow-exp14_Pinelli_1/601_RUN_correr_workflow_01.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_01.r")
-source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_01.r")
-source("~/labo2023ba/src/workflow-inicial/631_FE_historia_01.r")
-source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_01.r")
+source("~/labo2023ba/src/workflow-exp14_Pinelli_1/611_CA_reparar_dataset_01.r")
+source("~/labo2023ba/src/workflow-exp14_Pinelli_1/621_DR_corregir_drifting_01.r")
+source("~/labo2023ba/src/workflow-exp14_Pinelli_1/631_FE_historia_01.r")
+source("~/labo2023ba/src/workflow-exp14_Pinelli_1/641_TS_training_strategy_01.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_01.r")
-source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_01.r")
+source("~/labo2023ba/src/workflow-exp14_Pinelli_1/651_HT_lightgbm_01.r")
+source("~/labo2023ba/src/workflow-exp14_Pinelli_1/661_ZZ_final_01.r")
